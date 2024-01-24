00fe8bcded53c2b3ae09abbd28fe28e46a62b67a
Serhii Kliuch <skliu@softserveinc.com>
2024-01-24 11:56:51 +0200

    7

--- data/database.js
+++ data/database.js
@@ -15,6 +15,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
+
   console.log('Connection failed.');
   await client.close();
   console.log('Connection closed.');
